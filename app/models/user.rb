3363f78546027cc226a69e01fb5e791f60ed68ad
alpbourne <alpbourne@gmail.com>
2018-01-04 13:45:14 -0500

    added omniauth-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: %i[facebook]
   has_many :bucket_lists
 end
