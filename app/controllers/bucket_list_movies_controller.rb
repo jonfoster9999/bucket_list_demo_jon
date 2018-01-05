82c018fa401ea7589955156c664464264424ef32
alpbourne <alpbourne@gmail.com>
2018-01-05 16:03:42 -0500

    add movie to bucket list method

--- app/controllers/bucket_list_movies_controller.rb
+++ app/controllers/bucket_list_movies_controller.rb
@@ -1,18 +1,8 @@
 class BucketListMoviesController < ApplicationController
 
-  def create
-    if current_user
-      if @bucket_list = current_user.bucket_list
-        @bucket_list.add_movie(params[:movie_id])
-        @bucket_list.save
-      else @bucket_list = Bucket_list.create(user_id: current_user.id)
-        @bucket_list.add_movie(params[:movie_id])
-        @bucket_list.save
-      end
-      redirect_to user_bucket_lists_path
-    end
-    redirect_to movies_path
-  end 
+  def new
+    
+  end
 
 
 
--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -2,8 +2,4 @@ class BucketList < ApplicationRecord
   belongs_to :user
   has_many :bucket_list_movies
   has_many :movies, through: :bucket_list_movies
-
-  def add_movie(movie_id)
-    bucket_list_movies.build(movie_id: self.id, bucket_list_id: bucket_list_id, seen: default)
-  end
 end
