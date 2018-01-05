eaf5028880e55ad22e6d19f1341cca2fafd16673
alpbourne <alpbourne@gmail.com>
2018-01-05 11:33:12 -0500

    movie seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(name: 'The Godfather', genre: 'Drama', rating: 9.2)
+Movie.create(name: 'Schindlers List', genre: 'History', rating: 8.9)
+Movie.create(name: '12 Angry Men', genre: 'Drama', rating: 8.9)
+Movie.create(name: 'Life Is Beautiful', genre: 'Drama', rating: 8.6)
+Movie.create(name: 'The Good, the Bad, and the Ugly', genre: 'Western', rating: 8.9)
+Movie.create(name: 'The Shawshank Redemption', genre: 'Drama', rating: 9.3)
