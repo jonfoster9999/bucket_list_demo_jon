95bf3ce63e9d3981c83b328274ba6e547524b10c
alpbourne <alpbourne@gmail.com>
2018-01-05 16:13:51 -0500

    adding movie to bucket list

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,8 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @bucket_list = Bucket_List.find(params[:id])
+    @movie = Movie.find(params[:id])
   end
 
 end
