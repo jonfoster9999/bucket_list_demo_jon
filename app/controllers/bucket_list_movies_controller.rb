3edc2aadcf688415732cd53dec15e9ced94c435b
alpbourne <alpbourne@gmail.com>
2018-01-05 14:47:30 -0500

    add movie to bucketlist action

--- app/controllers/bucket_list_movies_controller.rb
+++ app/controllers/bucket_list_movies_controller.rb
@@ -3,9 +3,17 @@ class BucketListMoviesController < ApplicationController
   def create
     if current_user
       if @bucket_list = current_user.bucket_list
-        bucket_list_movies.build(movie_id: self.id, bucket_list_id: bucket_list_id, seen: default)
+        @bucket_list.add_movie(params[:movie_id])
+        @bucket_list.save
       else @bucket_list = Bucket_list.create(user_id: current_user.id)
-        bucket_list_movies.build(movie_id: self.id, bucket_list_id: bucket_list_id, seen: default)
+        @bucket_list.add_movie(params[:movie_id])
+        @bucket_list.save
+      end
+      redirect_to user_bucket_lists_path
+    end
+    redirect_to movies_path
+  end 
+
 
 
 
