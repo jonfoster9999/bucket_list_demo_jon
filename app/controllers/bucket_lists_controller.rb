35c4fc6a600f5d3acb721adacd35024f7030ee5e
alpbourne <alpbourne@gmail.com>
2018-01-08 14:30:36 -0500

    bucket list controller

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -10,4 +10,25 @@ class BucketListsController < ApplicationController
       @bucket_list.movies.build
     else
       flash[:alert] = "Please Log In First"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @bucket_list = Bucket_list.new(bucket_list_params)
+    if params[:bucket_list][:movie_ids] != "" && params[:bucket_list][:movie_ids] != nil
+      @bucket_list.movies << Movie.find(params[:bucket_list][:movie_ids])
+    end
+    if @bucket_list.save
+      redirect_to user_bucket_lists_path
+    else
+      flash[:alert] = "Please make sure all fields are filled out correctly"
+    end
+  end
+
+private
+
+  def bucket_list_params
+    params.require(:bucket_list).permit(:name, :movies_attributes [:name, :genre, :rating])
+  end
 end
