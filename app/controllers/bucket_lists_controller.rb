4d9e42a056e15cda122d4f5f077e67e00a9b5edd
alpbourne <alpbourne@gmail.com>
2018-01-08 14:13:45 -0500

    bucket list controller

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -1,2 +1,13 @@
 class BucketListsController < ApplicationController
+
+  def index
+    @bucket_lists = Bucket_list.all
+  end
+
+  def new
+    if current_user
+      @bucket_list = Bucket_list.new
+      @bucket_list.movies.build
+    else
+      flash[:alert] = "Please Log In First"
 end
