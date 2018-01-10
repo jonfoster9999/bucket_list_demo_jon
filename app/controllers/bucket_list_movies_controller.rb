62f28d599430fe0bb0b3df8b6aec37607be19364
alpbourne <alpbourne@gmail.com>
2018-01-10 10:06:36 -0500

    add movie form

--- app/controllers/bucket_list_movies_controller.rb
+++ app/controllers/bucket_list_movies_controller.rb
@@ -1,7 +1,7 @@
 class BucketListMoviesController < ApplicationController
 
   def new
-
+    @bucket_list_movie= BucketListMovie.new(bucket_list_id: params[:bucket_list_id], movie_id: params[:movie_id])
   end
 
 
