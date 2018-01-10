c52504160f27fbd21c5afedcf66cda0d0715e945
alpbourne <alpbourne@gmail.com>
2018-01-10 15:50:40 -0500

    add new bucket list form

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -16,17 +16,17 @@ class BucketListsController < ApplicationController
   end
 
   def create
-    @bucket_list = BucketList.new(bucket_list_params)
-    if params[:bucket_list][:movie_ids] != "" && params[:bucket_list][:movie_ids] != nil
-      @bucket_list.movies << Movie.find(params[:bucket_list][:movie_ids])
-    end
-    if @bucket_list.save
-      redirect_to user_bucket_lists_path
-    else
-      flash[:alert] = "Please make sure all fields are filled out correctly"
-      redirect_to new_user_bucket_list_path
-    end
-  end
+     @bucket_list = current_user.bucket_lists.build(bucket_list_params)
+     if params[:bucket_list][:movie_ids] != "" && params[:bucket_list][:movie_ids] != nil
+       @bucket_list.movies << Movie.find(params[:bucket_list][:movie_ids])
+     end
+     if @bucket_list.save
+       redirect_to user_bucket_lists_path(current_user, @bucket_list)
+     else
+       flash[:alert] = "Please make sure all fields are filled out correctly"
+       redirect_to new_user_bucket_list_path
+     end
+   end
 
 private
 
