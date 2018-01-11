4b09cc8398506337806df8f7f393939e547cab2e
alpbourne <alpbourne@gmail.com>
2018-01-11 13:01:14 -0500

    add movie to bucket list form

--- app/controllers/bucket_list_movies_controller.rb
+++ app/controllers/bucket_list_movies_controller.rb
@@ -5,10 +5,11 @@ class BucketListMoviesController < ApplicationController
   end
 
   def create
+    @bucket_list = BucketList.find(params[:bucket_list_id])
     if current_user
       @bucket_list_movie= BucketListMovie.new(bucket_list_id: params[:bucket_list_id], movie_id: params[:movie_id], seen: params[:seen])
       @bucket_list_movie.save
-      BucketList.movies(bucket_list_id: params[:bucket_list_id]) << @bucket_list_movie
+      @bucket_list.movies << @bucket_list_movie
       redirect_to user_bucket_lists_path(current_user)
     else
       flash[:alert] = "Please make sure all fields are filled out correctly"
