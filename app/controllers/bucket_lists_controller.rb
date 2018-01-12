8835af84787b2be1abe98c268ac69e60209e6863
alpbourne <alpbourne@gmail.com>
2018-01-12 10:46:23 -0500

    bucket list edit form/ view page

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -29,7 +29,8 @@ class BucketListsController < ApplicationController
    end
 
    def update
-
+     @bucket_list = Bucketlist.find(params[:id])
+     @bucket_list.update(bucket_list_params)
    end
 
 private
