627f34abcb93f6abf03afa262e141205c5d24149
alpbourne <alpbourne@gmail.com>
2018-01-12 10:22:44 -0500

    bucket list edit form

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -6,4 +6,8 @@ class BucketList < ApplicationRecord
   def movies_attributes=(movies_attributes)
     self.movies.build(movies_attributes[0])
   end
+
+  def bucket_list_movies_attributes=(bucket_list_movies_attributes)
+    self.bucket_list_movies.build(bucket_list_movies_attributes)
+  end
 end
