73f35b26bc937d4eabcb8dc09531b485d3dff1c6
alpbourne <alpbourne@gmail.com>
2018-01-14 16:23:38 -0500

    added snippet of code to NOTES.md

--- app/controllers/bucket_list_movies_controller.rb
+++ app/controllers/bucket_list_movies_controller.rb
@@ -9,7 +9,7 @@ class BucketListMoviesController < ApplicationController
     if current_user
       @bucket_list_movie= BucketListMovie.new(bucket_list_id: params[:bucket_list_id], movie_id: params[:movie_id], seen: params[:seen])
       @bucket_list_movie.save
-      @bucket_list.movie.all << @bucket_list_movie
+      @bucket_list.movies.all << @bucket_list_movie
       redirect_to user_bucket_lists_path(current_user)
     else
       flash[:alert] = "Please make sure all fields are filled out correctly"
