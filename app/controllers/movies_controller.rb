740799f72fcf41757864b7e297cb0e587c6ad771
alpbourne <alpbourne@gmail.com>
2018-01-19 10:47:56 -0500

    new movie controller methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,25 @@ class MoviesController < ApplicationController
     #@movie = Movie.find(params[:movie_id])
   end
 
+  def new
+    if current_user
+      @movie = Movie.new
+    else
+      flash[:alert] = "Please Log In First"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+     @movie = movie.build(movie_params)
+     if @movie.save
+       redirect_to movies_path
+     else
+       flash[:alert] = "Please make sure all fields are filled out correctly"
+       redirect_to new_movie_path
+     end
+   end
+
   def show
     @movies = Movie.all
     @highest_rated = @movies.where("rating > 8.5")
