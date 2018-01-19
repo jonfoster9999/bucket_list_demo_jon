5b36772dd80ff2495a436e2c38050ab1242306eb
Jonathan Foster <jonathan.foster@outfrontmedia.com>
2018-01-19 17:34:48 -0500

    changed stuff

--- app/controllers/bucket_list_movies_controller.rb
+++ app/controllers/bucket_list_movies_controller.rb
@@ -1,15 +1,15 @@
 class BucketListMoviesController < ApplicationController
 
   def new
+    @movie_id = params[:movie_id]
     @bucket_list_movie= BucketListMovie.new(bucket_list_id: params[:bucket_list_id], movie_id: params[:movie_id], seen: params[:seen])
   end
 
   def create
-    @bucket_list = BucketList.find_by(:id => params[:id])
-    if current_user
-      @bucket_list_movie= BucketListMovie.create(bucket_list_id: params[:bucket_list_id], movie_id: params[:movie_id], seen: params[:seen])
-      @bucket_list_movie.save
-      @bucket_list.movies.all << @bucket_list_movie
+      @bucket_list = BucketList.find_by(:id => params[:bucket_list_movie][:bucket_list_id])
+      if current_user
+      @bucket_list.movies << Movie.find(params[:movie_id])
+      @bucket_list.save
       redirect_to user_bucket_lists_path(current_user)
     else
       flash[:alert] = "Please make sure all fields are filled out correctly"
